db25f4caa77e6b1c5ee0a4a14ad9bda3547bf766
vreniz <vreniz@uninorte.edu.co>
2024-08-16 20:02:59 -0500

    cambios

--- Punto_1.js
+++
@@ -1,7 +0,0 @@
-function convertidorTemp(celsius) {
-    return celsius * (9/5) + 32;
-}
-
-// Example usage:
-let fahrenheit = convertidorTemp(23); // converts °C to Fahrenheit
-document.getElementById("output").innerText = fahrenheit; // Displays the result
